3c25e081974289e771cf4dc48e2cd2356e77af53
cpertile <c.pertile@icloud.com>
2023-12-15 14:25:01 -0300

    New config for CI/CD

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './App.tsx'
 import './index.css'
-import ContactPage from './pages/Contact/View/ContactPage.tsx'
 import CreateEditContactPage from './pages/Contact/CreateEdit/CreateEditContact.tsx'
+import ContactPage from './pages/Contact/View/ContactPage.tsx'
 import ErrorPage from './pages/Error/ErrorPage.tsx'
 import { Provider } from './store/Provider.tsx'
 
 const router = createBrowserRouter([
 	{
-		path: '/my-contacts',
+		path: '/*',
 		element: <App />,
 		errorElement: <ErrorPage />,
 		children: [
